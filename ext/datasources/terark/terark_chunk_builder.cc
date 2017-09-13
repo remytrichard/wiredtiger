91c4c539a256fbb593632b99173f77d49d740583
squalfof <squalfof@gmail.com>
2017-09-13 15:52:29 +0800

    Bug fix: done

--- ext/datasources/terark/terark_chunk_builder.cc
+++ ext/datasources/terark/terark_chunk_builder.cc
@@ -640,6 +640,7 @@ namespace terark {
 
 	void TerarkChunkBuilder::Abandon() {
 		closed_ = true;
+		zbuilder_->finish(DictZipBlobStore::ZipBuilder::FinishFreeDict);
 		zbuilder_.reset();
 		tmpIndexFile_.Delete();
 		tmpStoreFile_.Delete();
